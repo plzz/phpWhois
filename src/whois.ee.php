b43a4bef5a562cbcf77a2a4414d6f30c59d28c7d
Jussi Peltola <plz@plz.fi>
2017-01-11 08:05:39 +0200

    Initial whois.ee.php

---
+++ src/whois.ee.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @license http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2
+ * @license
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * @link http://phpwhois.pw
+ * @copyright Copyright (C)1999,2005 easyDNS Technologies Inc. & Mark Jeftovic
+ * @copyright Maintained by David Saez
+ * @copyright Copyright (c) 2014 Dmitry Lukashin
+ * @copyright Copyright (c) 2017 Jussi Peltola
+ */
+
+if (!defined('__EE_HANDLER__'))
+    define('__EE_HANDLER__', 1);
+
+require_once('whois.parser.php');
+
+class ee_handler {
+
+    function parse($data_str, $query) {
+        $translate = array(
+            'expire' => 'expires',
+            'registered' => 'created',
+            'nserver' => 'nserver',
+            'name' => 'name',
+            'email' => 'email',
+            'name' => 'name',
+        );
+
+        $contacts = array(
+            'Administrative contact' => 'admin',
+            'Technical contact' => 'tech',
+            'Registrant' => 'owner'
+        );
+
+        $r = array();
+        $r['regrinfo'] = generic_parser_a($data_str['rawdata'], $translate, $contacts, 'domain', 'ymd');
+
+        $r['regyinfo'] = array(
+            'referrer' => 'https://internet.ee',
+            'registrar' => 'EIS'
+        );
+
+        return $r;
+    }
+
+}
